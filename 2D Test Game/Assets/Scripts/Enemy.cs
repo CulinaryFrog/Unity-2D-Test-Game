1addf8e84463bf25af226f7e1919fef4998257cf
CulinaryFrog <jahyung.yun@gmail.com>
2022-11-28 02:47:31 -0500

    Began recreating scripts

---
+++ 2D Test Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+}
